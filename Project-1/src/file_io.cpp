e311064c7229dc23cf3ee82cd24f7fdc9a36a3d4
Silence2107 <pashka1201@gmail.com>
2022-09-08 15:32:53 +0200

    Custom libraries added

---
+++ Project-1/src/file_io.cpp
@@ -0,0 +1,20 @@
+
+#include "../include/file_io.h"
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+void two_columns_to_csv(const std::string& fpath, const std::vector<double>& x, const std::vector<double>& y, const std::string& separator)
+{
+    std::ofstream file(fpath);
+    if (x.size() != y.size())
+    {
+        throw std::invalid_argument("x and y must have the same size");
+    }
+    for (size_t i = 0; i < x.size(); ++i)
+    {
+        file << x[i] << separator << y[i] << '\n';
+    }
+}
