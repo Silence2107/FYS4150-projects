a57f6c67f690be3f79f7c99b6c9f1ab656f83086
Silence2107 <pashka1201@gmail.com>
2022-09-12 11:49:25 +0200

    Added support for column indexing into file_io

--- Project-1/include/file_io.h
+++ Project-1/include/file_io.h
@@ -5,11 +5,12 @@
 #include <string>
 #include <vector>
 
-/// @brief Write a csv file with the given data column by column. 
+/// @brief Write a csv file with the given data column by column.
 /// @param fpath The path to the file to write
 /// @param x First column of data
 /// @param y Second column of data
 /// @param separator The separator to use between columns. Defaults to ",".
-void two_columns_to_csv(const std::string& fpath, const std::vector<double>& x, const std::vector<double>& y, const std::string& separator = ",");
+/// @param index If true, write an index column. Default is false.
+void two_columns_to_csv(const std::string& fpath, const std::vector<double>& x, const std::vector<double>& y, const std::string& separator = ",", bool index = false);
 
 #endif
