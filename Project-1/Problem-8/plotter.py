bdf9945f5382c6d066a2f09ed1e6c4c7595ba35e
Gabriel Jonsson <gabriel.jonsson@allente.tv>
2022-09-12 21:54:44 +0200

    Cleanup python script.

--- Project-1/Problem-8/plotter.py
+++ Project-1/Problem-8/plotter.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import argparse 
 
-
+#Reusing plot script from problem 2, but with some adaptions to customize labels and title.
 parser = argparse.ArgumentParser()
 parser.add_argument('--csv')
 parser.add_argument('--y')
@@ -17,17 +17,11 @@ args = parser.parse_args()
 data = pd.read_csv(args.csv, header=None, sep='\t', names=['x', 'ux'])
 
 data.plot(x="x", y="ux", kind="line", figsize=(5, 5), legend="")
-#data.plot(x="x", y=args.y, kind="line", figsize=(5, 5), legend=args.legend)
 
-#plt.xlabel("x"); plt.ylabel("u(x)")
 plt.xlabel("x"); plt.ylabel(args.y)
 plt.title(args.title)
-#plt.xlim(0,1); plt.ylim(0 , .5)
-#plt.title("plot title")
-#plt.show()
-plt.savefig(args.output)
 
-# In[ ]:
+plt.savefig(args.output)
 
 
 
