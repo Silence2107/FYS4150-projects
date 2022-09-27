f76bf18d8ae84cadd8b49bf4c167f13d82ea0ee8
Silence2107 <pashka1201@gmail.com>
2022-09-27 15:15:50 +0200

    Problem 4 eigensolver implemented

--- Project-2/Problem-4/p2-4.cpp
+++ Project-2/Problem-4/p2-4.cpp
@@ -1,7 +1,6 @@
 
 #include "../include/create_tridiag.h"
 #include "../include/eigen_linalg.h"
-#include "../include/jacobi_rotate.h"
 
 #include <iostream>
 #include <armadillo>
@@ -15,10 +14,8 @@ int main()
     //double h = 0.1;
     int N = 6;
 
-    // initialize random
-    srand(time(0));
-
-    arma::mat R = arma::mat(N, N).eye();
+    // probe jacobi algorithm
+    
     // Generate random N*N matrix
     arma::mat A = arma::mat(N, N).randn();
 
@@ -26,45 +23,27 @@ int main()
     A = arma::symmatu(A);
 
     // compare with Armadillo
-    arma::vec eigval_arma;
-    arma::mat R_arma;
+    arma::vec eigval_arma, eigval_jacobi;
+    arma::mat R_arma, R_jacobi;
     arma::eig_sym(eigval_arma, R_arma, A);
 
     std::cout << "A:" << std::endl;
     std::cout << std::setprecision(4) << A << std::endl;
 
     double max;
-    // While max off diag value above threshold
-    do
-    {
-        size_t k, l;
-        //std::cout << A << std::endl;
-        max = fabs(abs_max_offdiag_for_symmetric(A, k, l));
-        if (max < eps)
-        {
-            break;
-        }
-        jacobi_rotate(A, R, k, l);
-
-    } while (true);
-
-    // get eigenvalues from the diag of A
-    arma::vec eigval = arma::vec(N);
-
-    for (int i = 0; i < N; i++)
-    {
-        eigval(i) = A(i, i);
-    }
+    
+    int iters; // number of iterations
+    bool conv; // convergence flag
+    jacobi_eigensolver(A, eps, eigval_jacobi, R_jacobi, 1000, iters, conv);
 
     // print
     std::cout << std::endl
               << "Eigenvalues from the Jacobi rotation method:" << std::endl;
-    std::cout << std::setprecision(4) << eigval << std::endl;
+    std::cout << std::setprecision(4) << eigval_jacobi << std::endl;
 
     std::cout << std::endl
               << "Eigenvectors from the Jacobi rotation method:" << std::endl;
-    std::cout << std::setprecision(4) << arma::normalise(R) << std::endl;
-
+    std::cout << std::setprecision(4) << arma::normalise(R_jacobi) << std::endl;
 
     std::cout << std::endl
               << "Eigenvalues from Armadillo:" << std::endl;
--- Project-2/src/eigen_linalg.cpp
+++ Project-2/src/eigen_linalg.cpp
@@ -4,6 +4,7 @@
 #include <armadillo>
 #include <stdexcept>
 #include <cmath>
+#include <stdexcept>
 
 double abs_max_offdiag_for_symmetric(const arma::mat &A, size_t &row, size_t &col)
 {
@@ -25,4 +26,116 @@ double abs_max_offdiag_for_symmetric(const arma::mat &A, size_t &row, size_t &co
             }
     // Now we may be sure max element is found
     return max;
+}
+
+void jacobi_rotate(arma::mat &A, arma::mat &R, size_t k, size_t l)
+{
+    // Obtain size directly from input matrix
+    int N = A.n_rows;
+    double t;
+
+    // new A and R matrices initialized to current A and R
+    arma::mat A_mp1 = A;
+    arma::mat R_mp1 = R;
+
+    // calculate tau, t, c, s
+    double tau = (A(l, l) - A(k, k)) / (2 * A(k, l));
+
+    if (tau > 0)
+    {
+        t = -tau + pow(1 + pow(tau, 2), .5);
+    }
+    else
+    {
+        t = -tau - pow(1 + pow(tau, 2), .5);
+    }
+
+    double c = 1 / (pow((1 + pow(t, 2)), 0.5));
+    double s = c * t;
+
+    // transform current A matrix: A^(m+1)=S_m^T*A^m*S_m, by updating elements
+    A_mp1(k, k) = A(k, k) * pow(c, 2) - 2 * A(k, l) * c * s + A(l, l) * pow(s, 2);
+    A_mp1(l, l) = A(l, l) * pow(c, 2) + 2 * A(k, l) * c * s + A(k, k) * pow(s, 2);
+    A_mp1(k, l) = 0;
+    A_mp1(l, k) = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (i != k && i != l)
+        {
+            A_mp1(i, k) = A(i, k) * c - A(i, l) * s;
+            A_mp1(k, i) = A_mp1(i, k);
+            A_mp1(i, l) = A(i, l) * c + A(i, k) * s;
+            A_mp1(l, i) = A_mp1(i, l);
+        }
+    }
+
+    // update rotation marix
+    for (int i = 0; i < N; i++)
+    {
+        R_mp1(i, k) = R(i, k) * c - R(i, l) * s;
+        R_mp1(i, l) = R(i, l) * c + R(i, k) * s;
+    }
+
+    // update A & R
+    A = A_mp1;
+    R = R_mp1;
+}
+
+void jacobi_eigensolver(const arma::mat &A, double eps, arma::vec &eigenvalues, arma::mat &eigenvectors,
+                        const int maxiter, int &iterations, bool &converged)
+{
+    // Obtain size directly from input matrix, and check it's square (could be optimized away later).
+    if (A.n_rows != A.n_cols)
+        throw std::invalid_argument("Matrix is not square");
+    int N = A.n_rows;
+
+    // initialize eigenvalues and eigenvectors
+    eigenvalues = arma::vec(N);
+    eigenvectors = arma::mat(N, N);
+
+    // initialize A and R matrices
+    arma::mat A_m = A;
+    arma::mat R_m = arma::eye(N, N);
+
+    // initialize iteration counter
+    iterations = 0;
+
+    size_t k, l; // indices of max off-diagonal element
+
+    // loop until max off-diagonal element is smaller than eps
+    while (true)
+    {
+        // initialize max off-diagonal element
+        double max_offdiag = fabs(abs_max_offdiag_for_symmetric(A_m, k, l));
+
+        if (max_offdiag < eps)
+        {
+            converged = true;
+            break;
+        }
+        if (iterations >= maxiter)
+        {
+            converged = false;
+            break;
+        }
+
+        // rotate A and R matrices
+        jacobi_rotate(A_m, R_m, k, l);
+
+        // update iteration counter
+        ++iterations;
+    }
+
+    // update eigenvalues and eigenvectors
+    for (int i = 0; i < N; i++)
+    {
+        eigenvalues(i) = A_m(i, i);
+        eigenvectors.col(i) = R_m.col(i);
+    }
+    
+    // sort eigenvalues and eigenvectors
+    arma::uvec indices = arma::sort_index(eigenvalues);
+    eigenvalues = eigenvalues.elem(indices);
+    eigenvectors = eigenvectors.cols(indices);
 }
