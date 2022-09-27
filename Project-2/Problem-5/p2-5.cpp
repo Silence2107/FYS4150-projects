d0a6c1052ad224f46873b4aabdaaf39957391da4
Gabriel Jonsson <gab.jonsson@gmail.com>
2022-09-27 18:44:22 +0200

    Code cleanup.

--- Project-2/Problem-5/p2-5.cpp
+++ Project-2/Problem-5/p2-5.cpp
@@ -1,7 +1,5 @@
-
 #include "../include/create_tridiag.h"
 #include "../include/eigen_linalg.h"
-//#include "../include/jacobi_rotate.h"
 
 #include <iostream>
 #include <armadillo>
@@ -32,44 +30,19 @@ int main(int argc, char **argv)
     // Generating matrix A that corresponds to the actual differential equation to solve, with step length as specified by parameters. 
     arma::mat A=create_tridiagonal(N, h);
 
-    // Symmetrize the matrix by reflecting the upper triangle to lower triangle
-    //A = arma::symmatu(A);
-
     // compare with Armadillo
     arma::vec eigval_arma;
     arma::mat R_arma;
     arma::eig_sym(eigval_arma, R_arma, A);
-/*
-    std::cout << "A:" << std::endl;
-    std::cout << std::setprecision(4) << A << std::endl;
-*/
 
     int actualIterations;
     bool converged;
         
-        // get eigenvalues from the diag of A
+    // get eigenvalues from the diag of A
     arma::vec eigval = arma::vec(N);
 
     jacobi_eigensolver(A, eps, eigval, R, maxIterations, actualIterations, converged);
-/*
-    // print
-    std::cout << std::endl
-              << "Eigenvalues from the Jacobi rotation method:" << std::endl;
-    std::cout << std::setprecision(4) << eigval << std::endl;
 
-    std::cout << std::endl
-              << "Eigenvectors from the Jacobi rotation method:" << std::endl;
-    std::cout << std::setprecision(4) << arma::normalise(R) << std::endl;
-
-
-    std::cout << std::endl
-              << "Eigenvalues from Armadillo:" << std::endl;
-    std::cout << std::setprecision(4) << eigval_arma << std::endl;
-
-    std::cout << std::endl
-              << "Eigenvectors from Armadillo:" << std::endl;
-    std::cout << std::setprecision(4) << R_arma << std::endl;
-*/
     std::cout << "Converged=" << converged << " after " << actualIterations << " iterations" << std::endl;
 
     return 0;
