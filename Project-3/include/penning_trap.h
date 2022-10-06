bc62825985838a4b440e91a43406f20393408948
Silence2107 <pashka1201@gmail.com>
2022-10-06 15:45:07 +0200

    Penning trap library (tested)

---
+++ Project-3/include/penning_trap.h
@@ -0,0 +1,72 @@
+#ifndef PENNING_TRAP_H
+#define PENNING_TRAP_H
+
+#include "particle.h"
+
+#include <functional>
+#include <vector>
+#include <armadillo>
+
+class PenningTrap
+{
+private:
+    /// @brief Magnetic field strength
+    double m_B0;    
+    /// @brief Voltage
+    double m_V0;   
+    /// @brief Characteristic distance between plates
+    double m_d;     
+    /// @brief Particles in the trap                                                
+    std::vector<Particle> m_particles;  
+    /// @brief Function that computes the interaction force from a first particle to a second particle.
+    /// Interaction is assumed to be between no more than 2-particles
+    std::function<arma::vec(const Particle &, const Particle &)> m_force;
+public:
+    /// @brief initializes the trap with the given parameters
+    /// @param B0 magnetic field strength
+    /// @param V0 voltage
+    /// @param d distance between plates
+    /// @param particles particles in the trap (absent by default)
+    PenningTrap(double B0, double V0, double d, const std::vector<Particle> &particles = std::vector<Particle>());
+    /// @brief specifies the force function
+    /// @param force force function from the first particle applied to the second particle
+    void enable_particle_interaction(const std::function<arma::vec(const Particle &, const Particle &)>& force);
+    /// @brief adds a particle to the trap
+    /// @param particle particle to add
+    void add_particle(const Particle &particle);
+
+    /// @brief access to the external electric field on the particle
+    const double &B0() const { return m_B0; };
+    /// @brief access to the external magnetic field on the particle
+    double &B0() { return m_B0; };
+    /// @brief access to the voltage
+    const double &V0() const { return m_V0; };
+    /// @brief access to the voltage
+    double &V0() { return m_V0; };
+    /// @brief access to the distance between plates
+    const double &d() const { return m_d; };
+    /// @brief access to the distance between plates
+    double &d() { return m_d; };
+    /// @brief access to the particles in the trap (read-only)
+    const std::vector<Particle> &particles() const { return m_particles; };
+
+    /// @brief calculates the external force field from the trap
+    /// @param r position of a particle for which the force is calculated
+    /// @param v velocity of a particle for which the force is calculated
+    /// @param q charge of a particle for which the force is calculated
+    /// @return external force field from the trap
+    const arma::vec external_force_field(const arma::vec &r, const arma::vec &v, double q) const;
+    /// @brief calculates the interaction force field on the particle in the trap
+    /// @param excluded_particle_index index of the particle in the trap, which we exclude from the calculation (the one that we calculate the force on) 
+    /// @param perturbed_r perturbed position of a particle for which the force is calculated
+    /// @param perturbed_v perturbed velocity of a particle for which the force is calculated
+    /// @return interaction force field on the particle
+    const arma::vec interaction_force_on_a_particle(size_t excluded_particle_index, const arma::vec &perturbed_r, const arma::vec &perturbed_v) const;
+    /// @brief calculates the total force field on the particle in the trap
+    /// @param excluded_particle_index index of the particle in the trap, which we exclude from the calculation (the one that we calculate the force on)
+    /// @param perturbed_particle particle for which the force is calculated (you may want to pass different positions and velocities here)
+    /// @return total force field on the particle
+    const arma::vec interaction_force_on_a_particle(size_t excluded_particle_index, const Particle &perturbed_particle) const;
+};
+
+#endif
---
+++ Project-3/src/penning_trap.cpp
@@ -0,0 +1,43 @@
+
+#include "../include/penning_trap.h"
+#include "../include/particle.h"
+
+#include <functional>
+#include <vector>
+#include <armadillo>
+
+PenningTrap::PenningTrap(double B0, double V0, double d, const std::vector<Particle> &particles)
+    : m_B0(B0), m_V0(V0), m_d(d), m_particles(particles){};
+
+void PenningTrap::enable_particle_interaction(const std::function<arma::vec(const Particle &, const Particle &)> &force)
+{
+    this->m_force = force;
+}
+
+void PenningTrap::add_particle(const Particle &particle)
+{
+    this->m_particles.push_back(particle);
+}
+
+const arma::vec PenningTrap::external_force_field(const arma::vec &r, const arma::vec &v, double q) const
+{
+    arma::vec F = arma::zeros<arma::vec>(3);
+    F(0) = q * (m_V0 / (m_d * m_d) * r(0) + m_B0 * v(2));
+    F(1) = q * (m_V0 / (m_d * m_d) * r(1) - m_B0 * v(1));
+    F(2) = -2 * q * m_V0 / (m_d * m_d) * r(2);
+    return F;
+}
+
+const arma::vec PenningTrap::interaction_force_on_a_particle(size_t excluded_particle_index, const Particle &perturbed_particle) const
+{
+    arma::vec F = arma::zeros<arma::vec>(3);
+    for (size_t i = 0; i < m_particles.size(); i++)
+        if (i != excluded_particle_index)
+            F += m_force(m_particles[i], perturbed_particle);
+    return F;
+}
+
+const arma::vec PenningTrap::interaction_force_on_a_particle(size_t excluded_particle_index, const arma::vec &perturbed_r, const arma::vec &perturbed_v) const
+{
+    return interaction_force_on_a_particle(excluded_particle_index, Particle(m_particles[excluded_particle_index].q, m_particles[excluded_particle_index].m, perturbed_r, perturbed_v));
+}
