caccc3de3029f776241f0cb8d0da50736c37ef7b
Gabriel Jonsson <gab.jonsson@gmail.com>
2022-10-20 01:06:16 +0200

    Two more files just to experiment with running the solvers. Will they give same result?

---
+++ Project-3/Problem-7/forward_euler_test2.cpp
@@ -0,0 +1,23 @@
+#include "../include/penning_trap.h"
+#include "../include/particle.h"
+
+#include <functional>
+#include <vector>
+#include <armadillo>
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+    PenningTrap trap(1, 1, 1);
+    Particle particle1(-5.3, 1, {-3.1, 1.2, -2.2}, {0, 0, 0}), particle2(4.2, 3, {1, 2.1, 1}, {-1, 1, -1});
+    // add a few particles
+    trap.add_particle(particle1);
+    trap.add_particle(particle2);
+
+	for(int i=0; i<100; i++)
+	{
+	//For now just evolve a few steps. Result will be printed. To be developed further.
+    trap.evolve_forward_Euler(0.00001);
+	}
+}
