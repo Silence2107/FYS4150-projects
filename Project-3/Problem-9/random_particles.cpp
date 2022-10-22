65803109fe354d92a568cf54a4ed05899ea8332a
deryaaricigil <deryaaricigil@live.no>
2022-10-22 12:12:56 +0200

    Missing time

--- Project-3/Problem-9/random_particles.cpp
+++ Project-3/Problem-9/random_particles.cpp
@@ -16,15 +16,10 @@ int main()
     //for (size_t i = 0; i < m_particles.size(); i++){
     //}
 
+    arma::vec r = arma::vec(3).randn()*0.1*trap.d();
+    arma::vec v = arma::vec(3).randn()*0.1*trap.d();
 
-    vec r = vec(3).randn()*0.1*trap.d();
-    //vec v = vec(3).randn()*0.1*trap.d();
-
-    //std::cout << arma::vec r << std::endl;
-
-
-
-    //Particle particle1(-5.3, 1, r, v), particle2(4.2, 3, {1, 2.1, 1}, {-1, 1, -1});
+    Particle particle1(-5.3, 1, r, v), particle2(4.2, 3, {1, 2.1, 1}, {-1, 1, -1});
     // add a few particles
     //trap.add_particle(particle1);
     //trap.add_particle(particle2);
