42f0ad4459173b63595ee4fd279274ce9cfd88b8
Silence2107 <pashka1201@gmail.com>
2022-10-22 18:56:37 +0200

    README improvement; added support for actual --csv parameter

--- Project-3/Problem-8/plotter3.py
+++ Project-3/Problem-8/plotter3.py
@@ -7,12 +7,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv')
-args = parser.parse_args("--csv euler.csv".split()) #empty if run from command line
+parser.add_argument(
+    '--csv', help='Input csv file. Defaults to particle1.csv', default='particle1.csv')
 
-data = pd.read_csv("particle1.csv", header=None, sep=",", names=['x', 'y'])
+data = pd.read_csv(parser.parse_args().csv, header=None,
+                   sep=",", names=['x', 'y'])
 data.plot(x="x", y="y", kind="line", figsize=(5, 5), legend="")
-plt.xlabel("x"); plt.ylabel("y")
+plt.xlabel("x")
+plt.ylabel("y")
 #plt.xlim(0,1); plt.ylim(0 , .5)
 #plt.title("plot title")
 plt.savefig('part1.pdf')
@@ -22,9 +24,6 @@ plt.savefig('part1.pdf')
 #plt.xlabel("x"); plt.ylabel("u(x)")
 ##plt.xlim(0,1); plt.ylim(0 , .5)
 ##plt.title("plot title")
-#plt.savefig('part2.pdf')
+# plt.savefig('part2.pdf')
 
 plt.show()
-
-
-
