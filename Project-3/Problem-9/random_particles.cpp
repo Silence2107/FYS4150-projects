761fd1cabcaa946abe965f9e66e8fa4f6b071653
deryaaricigil <deryaaricigil@live.no>
2022-10-23 04:25:44 +0200

    Regenerating and starting to build the file for problem 9

--- Project-3/Problem-9/random_particles.cpp
+++ Project-3/Problem-9/random_particles.cpp
@@ -1,5 +1,7 @@
 #include "../include/penning_trap.h"
-#include "../include/particle.hpp"
+#include "../include/particle.h"
+
+#include "../include/file_io.h"
 
 #include <functional>
 #include <vector>
@@ -13,17 +15,40 @@ int main()
 
     arma::arma_rng::set_seed(1000);
 
-    int particles = 2; // particle numbers
+    int particles = 1; //particle numbers
 
-    for (size_t i = 0; i < particles; i++)
-    {
+    //for printing
+    int endTime = 3;
+    double dt = 0.01;
+    double n = endTime / dt;
+    std::vector<double> x1(n + 1);
+    std::vector<double> y1(n + 1);
 
-        // generate random position and velocity according to a normal distribution
-        arma::vec r = arma::vec(3).randn() * 0.1 * trap.d();
-        arma::vec v = arma::vec(3).randn() * 0.1 * trap.d();
 
-        // generate particle and add it to the system
-        Particle particle(-5.3, 1, r, v);
-        trap.add_particle(particle);
+    for (size_t i = 0; i < particles; i++){
+
+      //generate random position and velocity according to a normal distribution
+      arma::vec r = arma::vec(3).randn()*0.1*trap.d();
+      arma::vec v = arma::vec(3).randn()*0.1*trap.d();
+
+      x1[0] = r(0);
+      y1[0] = r(1);
+
+      //generate particle and add it to the system
+      Particle particle(1, 1, r, v);
+      trap.add_particle(particle);
     }
-}
+
+
+    for (size_t i = 1; i < n; i++){
+        trap.evolve_forward_Euler_perturbed(dt, i, 1, 1);
+        x1[i] = trap.particles()[0].r[0];
+        y1[i] = trap.particles()[0].r[1];
+        }
+
+        //print particle numbers in the trap
+        std::cout << trap.particle_numbers() << std::endl;
+
+    two_columns_to_csv("particle1.csv", x1, y1, ",", false, 7);
+
+    }
