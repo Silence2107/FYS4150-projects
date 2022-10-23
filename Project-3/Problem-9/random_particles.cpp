2545cb16c2cc1e00d5c1ca3bff0d7f0eda6dbd1b
deryaaricigil <deryaaricigil@live.no>
2022-10-23 23:30:51 +0200

    results are 0

--- Project-3/Problem-9/random_particles.cpp
+++ Project-3/Problem-9/random_particles.cpp
@@ -12,9 +12,8 @@
 int main()
 {
 
-
   //DEFINE TIME
-  int endTime = 50;
+  int endTime = 500;
   double dt = 0.01;
   double n = endTime / dt;
 
@@ -29,7 +28,7 @@ int main()
 
     arma::arma_rng::set_seed(1000);
 
-    int particles = 5; //particle numbers
+    int particles = 100; //particle numbers
 
     std::vector<double> x1(n + 1);
     std::vector<double> y1(n + 1);
@@ -37,8 +36,8 @@ int main()
     for (size_t i = 0; i < particles; i++){
 
       //generate random position and velocity according to a normal distribution
-      arma::vec r = arma::vec(3).randn()*0.1*trap.d();
-      arma::vec v = arma::vec(3).randn()*0.1*trap.d();
+      arma::vec r = arma::vec(3).randu()*0.1*trap.d();
+      arma::vec v = arma::vec(3).randu()*0.1*trap.d();
 
       x1[0] = r(0);
       y1[0] = r(1);
@@ -58,21 +57,23 @@ int main()
     std::vector<double> fraction(116);           //fraction of particles
     //arma::vec fraction = arma::vec(size(wv));
 
-    //f = 0.1;  //AMPLITUDE
+    double f = 0;  //AMPLITUDE
 
     //calculations
-    //for (size_t i = 0; i < wv.size(); i++)
+    double V0_start = trap.V0();
+
     for (int i = 1; i < 117; i++){
       wv[i] = 0.02 + wv[i-1];
 
-      for (int j = 0; j < n; j++){
+      for (int j = 1; j <= n; j++){
         trap.evolve_forward_Euler(dt);
+        trap.V0() = V0_start * (1 + f*cos(wv[i-1]*j*dt));      //time dependent potential
       }
       //calculates fraction of particles in trap/ all the particles
-      fraction[i] = trap.particle_numbers()/particles;
+      fraction[i] = trap.particle_numbers(); ///(1.0*particles);
     }
 
     //write the values to file
-    two_columns_to_csv("fractions_amp_0.1.csv", wv, fraction, " ", false, 7);
+    two_columns_to_csv("fractions_euler_amp_0_4.csv", wv, fraction, " ", false, 7);
 
 }
