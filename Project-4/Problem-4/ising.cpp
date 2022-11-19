01ec9a77e89a1dfd9a39425b11bf5dbfb392fba8
jgjonsson <gab.jonsson@gmail.com>
2022-11-19 03:24:52 +0100

    Calculating average energy and magnetization.

--- Project-4/Problem-4/ising.cpp
+++ Project-4/Problem-4/ising.cpp
@@ -54,10 +54,10 @@ void performOneMonteCarloCycle(imat& A, size_t L, double beta, uniform_real_dist
 	//the changed spin makes this correct in this case. If we instead had looped over all lattices we would have only compared right and down instead.
 	//Side note: for the special case 2*2 lattice this still leads to double counting in principle, but we keep the algorithm equivalent regardless, 
 	//as this also makes most sense for the comparasion with analytical results. 
-	int spinLeft = A((randomRow-1)%L, randomCol);  //Modulus operator % makes the period boundary condition work, both ways.
-	int spinRight = A((randomRow+1)%L, randomCol);
-	int spinAbove = A(randomRow, (randomCol-1)%L);
-	int spinBelow = A(randomRow, (randomCol+1)%L);
+	int spinAbove = A((randomRow-1)%L, randomCol);  //Modulus operator % makes the period boundary condition work, both ways.
+	int spinBelow = A((randomRow+1)%L, randomCol);
+	int spinLeft = A(randomRow, (randomCol-1)%L);
+	int spinRight = A(randomRow, (randomCol+1)%L);
 	int sumOfNeightborSpins = spinLeft+spinRight+spinAbove+spinBelow;
 	//Recall that total energy is the sum of all products J*Sk*Sl with J coupling contant and Sk,Sl neighbors. 
 	//For local energy change, only take the sum of all products of flipped spin with it's 4 neighbor spins.
@@ -94,6 +94,48 @@ void performOneMonteCarloCycle(imat& A, size_t L, double beta, uniform_real_dist
 	cout << "Randomly updating (" << randomRow << "," <<  randomCol << ")." << endl;
 	cout << "Energy difference: " << energyDifference << " Flipped: " << (flipped>0?"yes":"no") << endl;
 }
+
+/*** Calculates energy averaged per site.
+ *   The energy is returned in units of coupling constant J. 
+ */
+double calculateAverageEnergy(imat& A, size_t L)
+{
+	long totalEnergy = 0;  //We do not need double until the division in the last step.
+	
+	//First we need the total energy of the system, calculated as sum of interaction energy of all sites.
+	//Loop over every single site in lattice and sum up energy due to interaction with neighbors. 
+	for (int i = 0; i < L; ++i)
+    {
+        for (int j = 0; j < L; ++j)
+        {
+			int thisSpin = A(i, j);
+			int spinBelow = A((i+1)%L, j);
+			int spinRight = A(i, (j+1)%L);
+			//The idea is similar to the partial energies in performOneMonteCarloCycle, but here we instead only
+			//calculate interactions going right and down, and thus avoid double counting. 
+			//The modulus operator % again make the periodic boundary condition work as it should. 
+			totalEnergy += thisSpin*(spinRight+spinBelow);
+        }
+    }
+	return ((double)totalEnergy) / (L*L);  //Divide with number of sites to get average.
+}
+
+double calculateAverageMagnetization(imat& A, size_t L)
+{
+	long totalSpin = 0;  //We do not need double until the division in the last step.
+	
+	//First we need the total energy of the system, calculated as sum of interaction energy of all sites.
+	//Loop over every single site in lattice and sum up energy due to interaction with neighbors. 
+	for (int i = 0; i < L; ++i)
+    {
+        for (int j = 0; j < L; ++j)
+        {
+			totalSpin += A(i, j);
+        }
+    }
+	return ((double)totalSpin) / (L*L);  //Divide with number of sites to get average.
+}
+
 
 int main()
 {
@@ -138,6 +180,9 @@ int main()
 
     std::cout << "After:" << std::endl;
     std::cout << std::setprecision(4) << latticeMatrix << std::endl;
+	
+	cout << "Average energy: " << calculateAverageEnergy(latticeMatrix, L) << endl;
+	cout << "Average magnetization: " << calculateAverageMagnetization(latticeMatrix, L) << endl;
 	
     return 0;
 }
