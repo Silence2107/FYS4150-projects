f48f4b80c99f27922ed4e25194ca88d3a07d0fa0
jgjonsson <gab.jonsson@gmail.com>
2022-11-27 09:25:00 +0100

    Now we have tests also for Crank Nicolson matrix B.

--- Project-5/Problem-2-3/crank_nicolson_unit_test.cpp
+++ Project-5/Problem-2-3/crank_nicolson_unit_test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace complex_literals;
 
-void compare_and_assert(string matrixName, arma::cx_mat &C, size_t i, size_t j, complex<double> expected)
+void compare_and_assert(string matrixName, const arma::cx_mat &C, size_t i, size_t j, complex<double> expected)
 {
     static const double epsilon = 1.0e-15;
 
@@ -25,14 +25,15 @@ void compare_and_assert(string matrixName, arma::cx_mat &C, size_t i, size_t j, 
     }
 }
 
-tuple<size_t, complex<double>> calc_a_k(const arma::mat &V, size_t i, size_t j, double dt, size_t Nx, complex<double> r)
+tuple<size_t, complex<double>, complex<double>> calc_a_k_b_k(const arma::mat &V, size_t i, size_t j, double dt, size_t Nx, complex<double> r)
 {
-    //By having another function here in the tests that also calculate a_k independent from the algoritm in the function we test, 
-    //we have some extra safe guard in detecting possible errors. (Either by typo in either file, or if future refactor accidently changes behaviour).
+    // By having another function here in the tests that also calculate a_k independent from the algoritm in the function we test,
+    // we have some extra safe guard in detecting possible errors. (Either by typo in either file, or if future refactor accidently changes behaviour).
     size_t k = flatten_index(i, j, Nx - 2);
     complex<double> a_k = 1.0 + 4.0 * r + 1i * dt * V(i, j) / 2.0;
-    cout << "k=" << k << ", ak=" << a_k << endl;
-    return make_tuple(k, a_k);
+    complex<double> b_k = 1.0 - 4.0 * r - 1i * dt * V(i, j) / 2.0;
+    cout << "k=" << k << ", ak=" << a_k << ", bk=" << b_k << endl;
+    return make_tuple(k, a_k, b_k);
 }
 
 int main()
@@ -42,7 +43,7 @@ int main()
     size_t Nx = 5, Ny = 5;
     arma::vec x_bound = {0, 1}, y_bound = {0, 1};
 
-    //Better randomize V_matr now, because if set to zeroes way to make elements in A and B are equal and tests could succeed by accident. 
+    // Better randomize V_matr now, because if set to zeroes way to make elements in A and B are equal and tests could succeed by accident.
     arma::mat V_matr = arma::mat(Nx - 2, Ny - 2, arma::fill::randu);
 
     double dt = 0.0001;
@@ -54,7 +55,7 @@ int main()
 
     double dx = (x_max - x_min) / (Nx - 1), dy = (y_max - y_min) / (Ny - 1);
 
-    auto [A, B] = generate_crank_nicolson_A_and_B(V_matr, dt, dx, dy, Nx, Ny);
+    const auto [A, B] = generate_crank_nicolson_A_and_B(V_matr, dt, dx, dy, Nx, Ny);
 
     // Testing a couple arbitrary values from matrces for expected values.
     // For now only one value from each matrix we know should be r and -r.
@@ -66,14 +67,18 @@ int main()
     compare_and_assert("B", B, 3, 0, r);
 
     // Calculate some a_k for more tests.
-    auto [k11, a11] = calc_a_k(V_matr, 1, 1, dt, Nx, r);
+    auto [k11, a11, b11] = calc_a_k_b_k(V_matr, 1, 1, dt, Nx, r);
+    auto [k21, a21, b21] = calc_a_k_b_k(V_matr, 2, 1, dt, Nx, r);
+    auto [k12, a12, b12] = calc_a_k_b_k(V_matr, 1, 2, dt, Nx, r);
+    auto [k20, a20, b20] = calc_a_k_b_k(V_matr, 2, 0, dt, Nx, r);
+    
     compare_and_assert("A", A, k11, k11, a11);
-
-    auto [k21, a21] = calc_a_k(V_matr, 2, 1, dt, Nx, r);
     compare_and_assert("A", A, k21, k21, a21);
-
-    auto [k12, a12] = calc_a_k(V_matr, 1, 2, dt, Nx, r);
     compare_and_assert("A", A, k12, k12, a12);
+    compare_and_assert("A", A, k20, k20, a20);
 
-    //TODO: Some tests for B as well
+    compare_and_assert("B", B, k11, k11, b11);
+    compare_and_assert("B", B, k21, k21, b21);
+    compare_and_assert("B", B, k12, k12, b12);
+    compare_and_assert("B", B, k20, k20, b20);
 }
