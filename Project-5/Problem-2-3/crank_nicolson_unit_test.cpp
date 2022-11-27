ecb31b6adf58d48e2cc5f3a5287f7b0984d73ca2
jgjonsson <gab.jonsson@gmail.com>
2022-11-27 05:14:39 +0100

    Refactored unit test to make it easier to add many more tests

--- Project-5/Problem-2-3/crank_nicolson_unit_test.cpp
+++ Project-5/Problem-2-3/crank_nicolson_unit_test.cpp
@@ -8,6 +8,23 @@
 using namespace std;
 using namespace std::complex_literals;
 
+void compare_and_assert(string matrixName, arma::cx_mat &C, int i, int j, std::complex<double> expected)
+{
+    static const double epsilon = 1.0e-15;
+
+    std::complex<double> actual = C(i, j);
+    double error = std::norm(expected - actual);
+
+    if (error < epsilon)
+    {
+        std::cout << "  Test ok!" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed unit test. Value at " << matrixName << "(" << i << "," << j << ")=" << actual << " expected: " << expected << std::endl;
+    }
+}
+
 int main()
 {
     // unit test for Crank Nicolson part of schrodinger_solver
@@ -27,19 +44,15 @@ int main()
 
     auto [A, B] = generate_crank_nicolson_A_and_B(V_matr, dt, dx, dy, Nx, Ny);
 
-    //Testing a couple arbitrary values from matrces for expected values. 
-    //For now only one value from each matrix we know should be r and -r. 
-    //r is defined as i*dt/(2h^2) or for rectangular case i*dt/(2dxdy)
-    //This could/should be extended to check more elemenets. 
-    std::complex<double> rExpected = 1i*dt/(2*dx*dy); //should be 8.000e-04 * 1i;
-
-    std::complex<double> error1 = rExpected - (-A(3, 0));
-    std::complex<double> error2 = rExpected - (B(3, 0));
-
-    double epsilon = 1.0e-15;
-
-    if (std::norm(error1) < epsilon && std::norm(error2) < epsilon)
-    {
-        std::cout << "  Test ok!" << std::endl;
-    }
+    // Testing a couple arbitrary values from matrces for expected values.
+    // For now only one value from each matrix we know should be r and -r.
+    // r is defined as i*dt/(2h^2) or for rectangular case i*dt/(2dxdy)
+    // This could/should be extended to check more elemenets.
+    std::complex<double> rExpected = 1i * dt / (2 * dx * dy); // should be 8.000e-04 * 1i;
+    /*
+        std::complex<double> error1 = rExpected - (-A(3, 0));
+        std::complex<double> error2 = rExpected - (B(3, 0));
+    */
+    compare_and_assert("A", A, 3, 0, -rExpected);
+    compare_and_assert("B", B, 3, 0, rExpected);
 }
