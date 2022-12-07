3625a7ffde8a8b2e0a2a53ef2ddcab6b2eb72bfe
jgjonsson <gab.jonsson@gmail.com>
2022-12-07 16:20:38 +0100

    Reordered plotting in Python script.

--- Project-5/Problem-6/plot_file.py
+++ Project-5/Problem-6/plot_file.py
@@ -1,8 +1,18 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+#Initial state
+# Read data from csv file into a np array
+data = np.genfromtxt('init_prob.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("init.png")
+
+#Final state
 # Read data from csv file into a np array
 data = np.genfromtxt('final_prob.csv', delimiter=',')
 
@@ -13,23 +23,13 @@ plt.imshow(data, cmap='hot', interpolation='nearest')
 plt.savefig("final.png")
 
 
+#Potential - can't be used right now as C++ does not export it.
 # Read data from csv file into a np array
-data = np.genfromtxt('potential.csv', delimiter=',')
+#data = np.genfromtxt('potential.csv', delimiter=',')
 
 # Plot the data
-plt.imshow(data, cmap='hot', interpolation='nearest')
+#plt.imshow(data, cmap='hot', interpolation='nearest')
 
 # Save the plot
-plt.savefig("potential.png")
-
-
-
-# Read data from csv file into a np array
-data = np.genfromtxt('init_prob.csv', delimiter=',')
-
-# Plot the data
-plt.imshow(data, cmap='hot', interpolation='nearest')
-
-# Save the plot
-plt.savefig("init.png")
+#plt.savefig("potential.png")
 
