62dc7a2df2b3903bc0ba73aa3960c165e792c8bb
jgjonsson <gab.jonsson@gmail.com>
2022-12-07 01:12:46 +0100

    Code for handling time steps, calculating probability for each step, and storing all calculated probabilities over x/y/t to Armadillo cube. After simulation, store the cube to file in binary format.

---
+++ Project-5/Problem-6/README.md
@@ -0,0 +1,9 @@
+# Problem 2-3 manual
+Solvers for Shrödinger equation with Crank Nicelson scheme. 
+
+# Usage
+Small programs for unit testing thereof. 
+- Compile the code with 
+	`g++ problem6.cpp -o problem6.exe ../src/* -I../include/ -std=c++17 -larmadillo`
+- Run the code with 
+	` ./problem6.exe`
--- Project-5/Problem-6/problem6.cpp
+++ Project-5/Problem-6/problem6.cpp
@@ -7,17 +7,18 @@
 
 int main()
 {
-    // unit test for initializing wave function. Then also running schrodinger_solver to check if it stays normalized. 
+    // unit test for initializing wave function. Then also running schrodinger_solver to check if it stays normalized.
 
-    size_t Nx = 10, Ny = 10;
+    size_t Nx = 10, Ny = 10, Nt;
     arma::vec x_bound = {0, 1}, y_bound = {0, 1};
     arma::mat V_matr = arma::zeros<arma::mat>(Nx - 2, Ny - 2);
 
-    double dt = 0.0001; // fine dt is REQUIRED for reasonable results, however be mindful about stability
+    double dt = 0.000025; // fine dt is REQUIRED for reasonable results, however be mindful about stability
+    double T = 0.008;     // End time of simulation.
 
-    //Initialize wave function psi.
-    //Picking some physical values for the wave packet. 
-    //For this unit test we could really use anything. However the numbers below are taken from an actual sample run we will do later.
+    // Initialize wave function psi.
+    // Picking some physical values for the wave packet.
+    // For this unit test we could really use anything. However the numbers below are taken from an actual sample run we will do later.
     arma::cx_vec psi_old = initialize_particle_wavefunction(Nx, Ny, x_bound, y_bound, 0.25, 0.5, 0.05, 0.05, 200.0, 0.0);
 
     // 1. check that the norm of the solution is preserved
@@ -50,8 +51,6 @@ int main()
     psi_new_mat.print("psi_new_mat = "); // should be at least a bit localized
     // check that the norm is preserved (should be close to 1 at reasonable dt and grid size)
     std::cout << "new_norm = " << arma::norm(psi_new) << std::endl;
-	
-	
 
     // 2. Dirichlet boundary conditions + 0 potential basically means that we simulate an infinite well
     // If we start with infinite well eigenstate, we should get the same eigenstate after some time, disregarding time phase
@@ -60,13 +59,17 @@ int main()
     std::cout << "\n Test 2. \n";
 
     Nx = 10, Ny = 10;
-    
-	arma::cube probabilities_all_time_steps = arma::cube(Nx - 2, Ny - 2, Nt);;
-	
+    Nt = T / dt;
+
+    // Setting up an Armadillo cube object, to store the probability in x/y-plane for each t.
+    arma::cube probabilities_all_time_steps = arma::cube(Nx - 2, Ny - 2, Nt);
+
     // disregard normalization as for now
-    auto lowest_eigenstate = [](double x, double y) { return std::sin(M_PI * x) * std::sin(M_PI * y); };
+    auto lowest_eigenstate = [](double x, double y)
+    { return std::sin(M_PI * x) * std::sin(M_PI * y); };
     // also, probe schrodinger_solver overload with a potential function instead of a vector! No need to discretize it
-    auto V_func = [](double x, double y) { return 0.0; };
+    auto V_func = [](double x, double y)
+    { return 0.0; };
     psi_old = arma::zeros<arma::cx_vec>((Nx - 2) * (Ny - 2));
     for (size_t i = 0; i < Nx - 2; i++)
     {
@@ -79,10 +82,25 @@ int main()
     psi_old = psi_old / arma::norm(psi_old);
     psi_new = psi_old;
     // perform time evolution
-    for (size_t i = 0; i < 10; ++i)
+    for (size_t t = 0; t < Nt; ++t)
     {
         psi_new = schrodinger_solver(psi_new, V_func, dt, Nx, Ny, x_bound, y_bound);
+
+        // calculate probability densities
+        arma::mat psi_new_prob = arma::zeros<arma::mat>(Nx - 2, Ny - 2);
+        for (size_t i = 0; i < Nx - 2; i++)
+        {
+            for (size_t j = 0; j < Ny - 2; j++)
+            {
+                psi_new_prob(i, j) = std::norm(psi_new(flatten_index(i, j, Nx - 2)));
+            }
+        }
+        probabilities_all_time_steps.slice(t) = psi_new_prob;
     }
+
+    // Save the cube in binary Armadillo format. This gives efficient storing of huge 3D data, and can be read by Python pyarma.
+    probabilities_all_time_steps.save("data3d.out");
+
     // calculate probability densities
     arma::mat psi_old_prob = arma::zeros<arma::mat>(Nx - 2, Ny - 2);
     arma::mat psi_new_prob = arma::zeros<arma::mat>(Nx - 2, Ny - 2);
@@ -94,10 +112,9 @@ int main()
             psi_new_prob(i, j) = std::norm(psi_new(flatten_index(i, j, Nx - 2)));
         }
     }
+
     // stats
     psi_old_prob.print("psi_old_prob = ");
     psi_new_prob.print("psi_new_prob = ");
-    std::cout << "avg. relative error = " << arma::accu(arma::abs(psi_old_prob - psi_new_prob) / psi_old_prob)/((Nx-2)*(Ny-2)) << std::endl;
-
-
+    std::cout << "avg. relative error = " << arma::accu(arma::abs(psi_old_prob - psi_new_prob) / psi_old_prob) / ((Nx - 2) * (Ny - 2)) << std::endl;
 }
