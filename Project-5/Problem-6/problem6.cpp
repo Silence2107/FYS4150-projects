2b2754c4703055cbc523335794fc36f43ac8cc9f
jgjonsson <gab.jonsson@gmail.com>
2022-12-07 17:06:14 +0100

    Refactored out a function perform_simulation to reduce size of main and make it cleared which parameteres we actually intend to vary.

--- Project-5/Problem-6/problem6.cpp
+++ Project-5/Problem-6/problem6.cpp
@@ -7,6 +7,23 @@
 
 using namespace std;
 
+int perform_simulation(int number_slits, double T, size_t grid_size);
+double hard_coded_slit(double x, double y, int number_of_slits);
+
+int main()
+{
+    // TODO: These things would be better to parametrize with argc,argv.
+
+    double T = 0.008; // Short end time of simulation, this is for Problem 7.
+    // double T = 0.02; // Longer end time of simulation, this is for Problem 8.
+
+    int number_of_slits = 2; // Default value, it will be relevant to use 0, 1 and 3 later.
+    //size_t grid_size = 200;  // 200 for good accuracy, but will take long time. Consider 50 or 100 for quicker tests. Assuming square, not worrying about rectangle grid for now.
+    size_t grid_size = 100;
+
+    return perform_simulation(number_of_slits, T, grid_size);
+}
+
 double hard_coded_slit(double x, double y, int number_of_slits)
 {
     // initialize potential to simulate wall
@@ -19,34 +36,41 @@ double hard_coded_slit(double x, double y, int number_of_slits)
     return many_slit_potential(x, y, x_center, y_center, x_slit_width, y_slit_width, y_slit_spacing, number_of_slits, wall_potential);
 }
 
-int main()
+int perform_simulation(int number_of_slits, double T, size_t grid_size)
 {
-    // unit test for initializing wave function. Then also running schrodinger_solver to check if it stays normalized.
+    // Adding 1, so for grid of 200 steps, use matrix of 201*201
+    size_t Nx = grid_size + 1, Ny = grid_size + 1;
 
-    size_t Nx = 101, Ny = 101;
     arma::vec x_bound = {0, 1}, y_bound = {0, 1};
 
     double dt = 0.000025; // fine dt is REQUIRED for reasonable results, however be mindful about stability
-    // double T = 0.008;     // End time of simulation.
-    double T = 0.008; // Short end time of simulation, this is for Problem 7.
-    //double T = 0.02; // Longer end time of simulation, this is for Problem 8.
     size_t Nt = 1 + T / dt;
 
     cout << "Iteration number to run: " << Nt << endl;
 
     // Making a function pointer to potential function, using function above with hardcoded values, except number of slits.
-    auto V_func = [](double x, double y)
-    { return hard_coded_slit(x, y, 2); };
-    //{ return 0.0; };
+    auto V_func =
+        [&number_of_slits](double x, double y)
+    {
+        if (number_of_slits == 0)
+        {
+            // Number of slits zero is used to mean no wall (so not a completely blocking wall).
+            // This is intended for the part of Problem 7 when we are asked to run without potential.
+            return 0.0;
+        }
+        else
+        {
+            // For non-zero number of slits we calculate an actual potential.
+            return hard_coded_slit(x, y, number_of_slits);
+        }
+    };
 
     // Initialize wave function psi.
     // Picking some physical values for the wave packet.
     // For this unit test we could really use anything. However the numbers below are taken from an actual sample run we will do later.
     arma::cx_vec psi_old = initialize_particle_wavefunction(Nx, Ny, x_bound, y_bound, 0.25, 0.5, 0.05, 0.05, 200.0, 0.0);
-    //Parameter list meaning x_center=0.25, y_center=0.5, spread x=0.05, spread y=0.05, px=200, py=0.
+    // Parameter list meaning x_center=0.25, y_center=0.5, spread x=0.05, spread y=0.05, px=200, py=0.
 
-    // 1. check that the norm of the solution is preserved
-    std::cout << "\n Test 1. \n";
     // print psi_old as matrix
     arma::cx_mat psi_old_mat = arma::zeros<arma::cx_mat>(Nx - 2, Ny - 2);
     for (size_t i = 0; i < Nx - 2; i++)
@@ -90,8 +114,14 @@ int main()
         {
             psi_old_prob(i, j) = std::norm(psi_old(flatten_index(i, j, Nx - 2)));
             psi_new_prob(i, j) = std::norm(psi_new(flatten_index(i, j, Nx - 2)));
+
+            //Squaring the results since norm is square root of the value we want: psi conjugate*psi
+            psi_old_prob(i, j) = (psi_old_prob(i, j)*psi_old_prob(i, j));
+            psi_new_prob(i, j) = (psi_new_prob(i, j)*psi_new_prob(i, j));
         }
     }
+
+    std::cout << "new_norm = " << arma::norm(psi_new) << std::endl;
 
     // save the matrix to a csv file
     psi_old_prob.save("init_prob.csv", arma::csv_ascii);
@@ -101,4 +131,6 @@ int main()
     // psi_old_prob.print("psi_old_prob = ");
     // psi_new_prob.print("psi_new_prob = ");
     std::cout << "avg. relative error = " << arma::accu(arma::abs(psi_old_prob - psi_new_prob) / psi_old_prob) / ((Nx - 2) * (Ny - 2)) << std::endl;
+
+    return 0;
 }
