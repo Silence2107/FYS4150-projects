5018413c6b296163fe296f96b0dda63f6f7d3d7a
jgjonsson <gab.jonsson@gmail.com>
2022-12-14 15:55:57 +0100

    Adding python script for plotting and README-file.

---
+++ Project-5/Problem-9/README.md
@@ -0,0 +1,11 @@
+# Problem 9 manual
+#Run simulations for different number of slits, and plot probabilities for detecting at screen, in 1D form.
+
+# Usage
+Small programs for unit testing thereof. 
+- Compile the code with 
+	`g++ problem9.cpp -o problem9.exe ../src/* -I../include/ -std=c++17 -larmadillo`
+- Run the code with 
+	`./problem9.exe`
+	`python3 ./plot_prob_of_y.py --file screen.csv`
+Then look at generated .pdf files. 
---
+++ Project-5/Problem-9/plot_prob_of_y.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Plots the probability of y')
+parser.add_argument('--file', type=str, help='The file to read the data from')
+
+args = parser.parse_args()
+
+# Read the data from a file
+df = pd.read_csv(args.file, sep=',', header=None, names=['y', 'prob'])
+
+# Plot the data
+df.plot(x='y', y='prob', kind='line', legend=False)
+plt.xlabel('y, rel.u.')
+plt.ylabel('Tot. probability')
+
+# Save the plot
+plt.savefig('prob_of_y.pdf')
