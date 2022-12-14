5a6250b0146890f018a3a2ff67f7cd688c786965
deryaaricigil <deryaaricigil@live.no>
2022-12-14 18:43:46 +0100

    corrected font, added axes

--- Project-5/Problem-5/plot_file.py
+++ Project-5/Problem-5/plot_file.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+#PLOT VISUALS
+plt.figure(figsize=(5, 5))
+plt.rcParams.update({'font.size': 12})
+plt.xlabel('x'); plt.ylabel('y');
+
 # Read data from csv file into a np array
 data = np.genfromtxt('data.csv', delimiter=',')
 
