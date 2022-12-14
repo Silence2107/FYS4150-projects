a6518bb215095a3b8b43fd1ec8093d30a3a2e277
jgjonsson <gab.jonsson@gmail.com>
2022-12-14 12:18:30 +0100

    Adding C++ and Python code to also export and plot real and imaginary part of wave function.

--- Project-5/Problem-8/plot_many_timesteps.py
+++ Project-5/Problem-8/plot_many_timesteps.py
@@ -29,3 +29,64 @@ plt.imshow(data, cmap='hot', interpolation='nearest')
 
 # Save the plot
 plt.savefig("prob80.pdf")
+
+
+
+#Initial state
+# Read data from csv file into a np array
+data = np.genfromtxt('real0.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("real0.pdf")
+
+# Read data from csv file into a np array
+data = np.genfromtxt('real40.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("real40.pdf")
+
+# Read data from csv file into a np array
+data = np.genfromtxt('real80.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("real80.pdf")
+
+
+
+
+#Initial state
+# Read data from csv file into a np array
+data = np.genfromtxt('imag0.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("imag0.pdf")
+
+# Read data from csv file into a np array
+data = np.genfromtxt('imag40.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("imag40.pdf")
+
+# Read data from csv file into a np array
+data = np.genfromtxt('imag80.csv', delimiter=',')
+
+# Plot the data
+plt.imshow(data, cmap='hot', interpolation='nearest')
+
+# Save the plot
+plt.savefig("imag80.pdf")
